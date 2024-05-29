26487431c608f9e643cac7c8490f2cb76941750c
khaihanhtang <84257457+khaihanhtang@users.noreply.github.com>
2024-05-29 13:35:00 +0800

    Initiate project

---
+++ main.py
@@ -0,0 +1,18 @@
+import string
+import sys
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+token: string = input("Enter bot token: ")
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token(token).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
